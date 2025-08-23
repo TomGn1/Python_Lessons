ed8eb7e43d5bb58e3213e429c2af9104a0c30b6f
Tom Guenin <guenin.tom@gmail.com>
2025-08-23 19:16:52 +0200

    ajout d'un système de score

--- nb_mystere_score.py
+++ nb_mystere_score.py
@@ -3,7 +3,9 @@ import random
 
 print("Bienvenue dans le jeu du nombre mystère !")
 
-
+ancien_joueur = "personne"
+dernier_score = None
+meilleur_score = 0
 oui = "oui"
 non = "non"
 rejouer = oui
@@ -25,6 +27,19 @@ while rejouer == oui:
                 f"Bravo ! Tu as trouvé le nombre mystère {nbMystere} "
                 f"en {nbTentatives} tentatives."
             )
+
+            if meilleur_score == 0 or nbTentatives < meilleur_score:
+                meilleur_score = nbTentatives
+                meilleur_joueur = input("Quel est ton nom ? ")
+                print(f"Félicitations {meilleur_joueur} ! "
+                      f"Tu as le nouveau meilleur score avec {meilleur_score} tentatives.")
+                if meilleur_joueur != ancien_joueur and dernier_score is not None:
+                    print(f"Le meilleur score à battre était de {dernier_score} "
+                          f"tentatives par {ancien_joueur}.")
+                ancien_joueur = meilleur_joueur
+                dernier_score = nbTentatives
+            else:
+                print(f"Le meilleur score actuel est de {meilleur_score} ")
 
     rejouer = input(f"Voulez vous rejouer ? ({oui}/{non})")
 
