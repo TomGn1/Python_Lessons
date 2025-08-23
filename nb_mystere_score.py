a3315213a2e58b1ddf022ab28a3176b48d7ec4ca
Tom Guenin <guenin.tom@gmail.com>
2025-08-23 10:08:18 +0200

    ajout d'une version simplifié du script nb_mystre

---
+++ nb_mystere_score.py
@@ -0,0 +1,34 @@
+import random
+
+
+print("Bienvenue dans le jeu du nombre mystère !")
+
+
+oui = "oui"
+non = "non"
+rejouer = oui
+while rejouer == oui:
+    nbTentatives = 0
+    nbMystere = random.randint(1, 50)
+    print("Tu dois deviner le nombre mystère entre 1 et 50.")
+    nombre = int(input("Quel est le nombre mystère ? "))
+    while nombre != nbMystere:
+        nbTentatives += 1
+        if nombre < nbMystere:
+            print("C'est plus !")
+            nombre = int(input("Quel est le nombre mystère ? "))
+        else:
+            print("C'est moins !")
+            nombre = int(input("Quel est le nombre mystère ? "))
+        if nombre == nbMystere:
+            print(
+                f"Bravo ! Tu as trouvé le nombre mystère {nbMystere} "
+                f"en {nbTentatives} tentatives."
+            )
+
+    rejouer = input(f"Voulez vous rejouer ? ({oui}/{non})")
+
+
+if rejouer == non:
+    print("Merci d'avoir joué !")
+    exit()
