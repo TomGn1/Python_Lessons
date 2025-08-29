d6f1125b95aaefba155a1f00fb21b173fc8bc2e3
Tom Guenin <guenin.tom@gmail.com>
2025-08-29 13:32:52 +0200

    no change

--- cloneur.sh
+++ cloneur.sh
@@ -1,10 +1,23 @@
 #!/bin/bash
 
-original=/home/user/dossier_original/*
-copies=/home/user/dossier_copies/
+if [ $# -lt 2 ]; then
+    echo "Merci de fournir 2 arguments à ce script."
+    echo "Utilisation : ./cloneur.sh /chemin/vers/dossier_original /chemin/vers/dossier_copie"
+    exit 1
+fi
+
+original=$1
+copies=$2
+
+if [ ! -d $original ]; then
+    echo "Erreur, $original n'est pas un dossier valide."
+    exit 2
+fi
 
 if [ ! -d "$copies" ]; then
     mkdir $copies
+    cp -rv $original/* $copies
+else
+    cp -rv $original/* $copies
 fi
 
-cp $original $copies
